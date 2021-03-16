ded943e44bbd846e68d73a5703d0739f24c79a4e
Yusuf GUNER <yusuf_guner@msn.com>
2021-03-16 15:46:50 +0100

    Add springboot project

---
+++ src/main/java/zerogaspi/GaspiBootApplication.java
@@ -0,0 +1,13 @@
+package zerogaspi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaspiBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaspiBootApplication.class, args);
+	}
+
+}

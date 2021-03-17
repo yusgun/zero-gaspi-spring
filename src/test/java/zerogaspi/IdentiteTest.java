53e1c76798b36fe8e148a4f6e8beb81934843b76
Yusuf GUNER <yusuf_guner@msn.com>
2021-03-17 09:36:31 +0100

    Ajout du test de connexion pour l'association

--- src/test/java/zerogaspi/IdentiteTest.java
+++ src/test/java/zerogaspi/IdentiteTest.java
@@ -1,14 +1,19 @@
 package zerogaspi;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import zerogaspi.dao.IConnexion;
 import zerogaspi.dao.IIdentite;
+import zerogaspi.model.Association;
 import zerogaspi.model.Client;
 import zerogaspi.model.Connexion;
+import zerogaspi.model.Entreprise;
 import zerogaspi.model.Identite;
+import zerogaspi.model.Vendeur;
 
 @SpringBootTest
 public class IdentiteTest {
@@ -25,15 +30,22 @@ public class IdentiteTest {
 	 */
 	public void identifiantConnexion() {
 		// Création de la connexion et de l'identite
-		Connexion connexion = new Connexion("alphonse.denis@email.com", "0123456");
-		Identite identite = new Client("0606060606", "5 rue Jules Ferry", "59000", "Lille", "DENIS", "Alphonse", connexion, 20);
+		Connexion connexion1 = new Connexion("alphonse.denis@email.com", "0123456");
+		Identite identite1 = new Client("0606060606", "5 rue Jules Ferry", "59000", "Lille", "DENIS", "Alphonse", connexion1, 20);
+		Connexion connexion2 = new Connexion("AntoinePatenaude@armyspy.com", "Ahgho2aeph9");
+		Identite identite2 = new Association("0108132116", "11 rue du 25 décembre", "57000", "Metz", "Patenaude", "Antoine", connexion2, 15);
 		// Ajout à la base
-		connexionDao.save(connexion);
-		identiteDao.save(identite);
+		connexionDao.save(connexion1);
+		identiteDao.save(identite1);
+		connexionDao.save(connexion2);
+		identiteDao.save(identite2);
 		// Recherche de la connexion de l'utilisateur
-		Connexion findConnexion = identiteDao.findCredentialsById(identite.getId());
+		Connexion findConnexion1 = identiteDao.findCredentialsById(identite1.getId());
+		Connexion findConnexion2 = identiteDao.findCredentialsById(identite2.getId());
 		// Lancement des tests...
-		assertEquals("alphonse.denis@email.com", findConnexion.getMail());
-		assertEquals("0123456", findConnexion.getMotDePasse());
+		assertEquals("alphonse.denis@email.com", findConnexion1.getMail());
+		assertEquals("0123456", findConnexion1.getMotDePasse());
+		assertEquals("AntoinePatenaude@armyspy.com", findConnexion2.getMail());
+		assertEquals("Ahgho2aeph9", findConnexion2.getMotDePasse());
 	}
 }

1a59cf1ead6e1a50c4d931d2fb7dca1000faa9f7
bilel <bilel.boukherbache@outlook.fr>
2021-03-17 15:20:40 +0100

    a

--- src/main/java/zerogaspi/dao/IFavoris.java
+++ src/main/java/zerogaspi/dao/IFavoris.java
@@ -1,19 +1,26 @@
 package zerogaspi.dao;
 
-import java.util.Optional;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import zerogaspi.model.Client;
 import zerogaspi.model.Entreprise;
 import zerogaspi.model.ListeFavori;
 
 public interface IFavoris extends JpaRepository<ListeFavori, Long> {
 
 	
-	@Query("select l.nomEntreprise from ListeFavoris l join Client  where c.id = :id")
+	@Query("select lf.entreprise from ListeFavori lf where lf.client = :client")
+	List<Entreprise>findByClient(@Param("client") Client client);
 	
-	 find(@Param("id") Long id);
+	
+	//@Query("select lf.entreprise from ListeFavori lf where lf.client = :client")
+	//List<Entreprise>findByClient(@Param("client") Client client);
+	
+	//@Query("select lf.entreprise from ListeFavori lf where lf.client = :client")
+	//List<Entreprise>findByClient(@Param("client") Client client);
 	
 }
---
+++ src/test/java/zerogaspi/FavorisTest.java
@@ -0,0 +1,17 @@
+package zerogaspi;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import zerogaspi.dao.IFavoris;
+
+@SpringBootTest
+public class FavorisTest {
+	
+	@Autowired
+	IFavoris favorisDao;
+	
+	
+	
+
+}

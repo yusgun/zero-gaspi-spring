68a4fc22008e3f53559e676cfbaa7667067da666
Valere <valeregbv1@gmail.com>
2021-03-18 12:01:31 +0100

    Ajout des requêtes Commande sur la recherche d'historique.

--- src/main/java/zerogaspi/dao/ICommande.java
+++ src/main/java/zerogaspi/dao/ICommande.java
@@ -11,7 +11,22 @@ import zerogaspi.model.Commande;
 public interface ICommande extends JpaRepository<Commande, Long> {
 
 	@Query("select c from Commande c where c.dateEnvoie = :dateEnvoie order by c.dateEnvoie desc ")
-	List<Commande> findByDateEnvoie(@Param("dateEnvoie") Date dateEnvoie);
+	List<Commande> findByDateEnvoieDesc(@Param("dateEnvoie") Date dateEnvoie);
+	
+	@Query("select c from Commande c where c.dateEnvoie = :dateEnvoie order by c.dateEnvoie Asc ")
+	List<Commande> findByDateEnvoieAsc(@Param("dateEnvoie") Date dateEnvoie);
+	
+	@Query("select c from Commande c where c.datePaiement = :datePaiement order by c.datePaiement Desc ")
+	List<Commande> findByDatePaiementDesc(@Param("datePaiement") Date datePaiement);
+	
+	@Query("select c from Commande c where c.datePaiement = :datePaiement order by c.datePaiement Asc ")
+	List<Commande> findByDatePaiementAsc(@Param("datePaiement") Date datePaiement);
+	
+	@Query("select c from Commande c where c.dateArrivee = :dateArrivee order by c.dateArrivee Desc ")
+	List<Commande> findByDateArriveeDesc(@Param("dateArrivee") Date dateArrivee);
+	
+	@Query("select c from Commande c where c.dateArrivee = :dateArrivee order by c.dateArrivee Asc ")
+	List<Commande> findByDateArriveeAsc(@Param("dateArrivee") Date dateArrivee);
 
 
 }
--- src/test/java/zerogaspi/CommandeTest.java
+++ src/test/java/zerogaspi/CommandeTest.java
@@ -19,18 +19,11 @@ public class CommandeTest {
 	@Autowired
 	private ICommande commandeDao;
 
-	boolean isSorted(int[] array) {
-		for (int i = 1; i < array.length; ++i)
-			if (array[i] < array[i - 1])
-				return false;
-		return true;
-	}
-
 	@Test
-	public void findByDateEnvoie() throws ParseException {
+	public void findByDateEnvoieDesc() throws ParseException {
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
-		List<Commande> commandes2 = commandeDao.findByDateEnvoie(sdf.parse("28/03/2021"));
+		List<Commande> commandes2 = commandeDao.findByDateEnvoieDesc(sdf.parse("28/03/2021"));
 		
 		
 		Commande commande = new Commande(sdf.parse("28/03/2021"), sdf.parse("28/03/2021"), sdf.parse("29/03/2021"),
@@ -45,9 +38,135 @@ public class CommandeTest {
 				null);
 		commande2 = commandeDao.save(commande2);
 
-		List<Commande> commandes = commandeDao.findByDateEnvoie(sdf.parse("28/03/2021"));
+		List<Commande> commandes = commandeDao.findByDateEnvoieDesc(sdf.parse("28/03/2021"));
 		assertEquals(3, commandes.size() - commandes2.size());
 
 	}
+	
+	@Test
+	public void findByDateEnvoieAsc() throws ParseException {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		List<Commande> commandes2 = commandeDao.findByDateEnvoieAsc(sdf.parse("28/03/2021"));
+		
+		
+		Commande commande = new Commande(sdf.parse("30/03/2021"), sdf.parse("28/03/2021"), sdf.parse("29/03/2021"),
+				null);
+		commande = commandeDao.save(commande);
+
+		Commande commande1 = new Commande(sdf.parse("30/03/2021"), sdf.parse("28/03/2021"), sdf.parse("29/03/2021"),
+				null);
+		commande1 = commandeDao.save(commande1);
+
+		Commande commande2 = new Commande(sdf.parse("30/03/2021"), sdf.parse("28/03/2021"), sdf.parse("29/03/2021"),
+				null);
+		commande2 = commandeDao.save(commande2);
+
+		List<Commande> commandes = commandeDao.findByDateEnvoieAsc(sdf.parse("28/03/2021"));
+		assertEquals(3, commandes.size() - commandes2.size());
+
+	}
+	
+	@Test
+	public void findByDatePaiementDesc() throws ParseException {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		List<Commande> commandes2 = commandeDao.findByDatePaiementDesc(sdf.parse("30/03/2021"));
+		
+		
+		Commande commande = new Commande(sdf.parse("30/03/2021"), sdf.parse("28/03/2021"), sdf.parse("29/03/2021"),
+				null);
+		commande = commandeDao.save(commande);
+
+		Commande commande1 = new Commande(sdf.parse("30/03/2021"), sdf.parse("28/03/2021"), sdf.parse("29/03/2021"),
+				null);
+		commande1 = commandeDao.save(commande1);
+
+		Commande commande2 = new Commande(sdf.parse("30/03/2021"), sdf.parse("28/03/2021"), sdf.parse("29/03/2021"),
+				null);
+		commande2 = commandeDao.save(commande2);
+
+		List<Commande> commandes = commandeDao.findByDatePaiementDesc(sdf.parse("30/03/2021"));
+		System.out.println(commandes);
+		assertEquals(3, commandes.size() - commandes2.size());
+
+	}
+	
+	@Test
+	public void findByDatePaiementAsc() throws ParseException {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		List<Commande> commandes2 = commandeDao.findByDatePaiementAsc(sdf.parse("30/03/2021"));
+		
+		
+		Commande commande = new Commande(sdf.parse("30/03/2021"), sdf.parse("28/03/2021"), sdf.parse("29/03/2021"),
+				null);
+		commande = commandeDao.save(commande);
+
+		Commande commande1 = new Commande(sdf.parse("30/03/2021"), sdf.parse("28/03/2021"), sdf.parse("29/03/2021"),
+				null);
+		commande1 = commandeDao.save(commande1);
+
+		Commande commande2 = new Commande(sdf.parse("30/03/2021"), sdf.parse("28/03/2021"), sdf.parse("29/03/2021"),
+				null);
+		commande2 = commandeDao.save(commande2);
+
+		List<Commande> commandes = commandeDao.findByDatePaiementAsc(sdf.parse("30/03/2021"));
+		System.out.println(commandes);
+		assertEquals(3, commandes.size() - commandes2.size());
+
+	}
+	
+	@Test
+	public void findByDateArriveeDesc() throws ParseException {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		List<Commande> commandes2 = commandeDao.findByDateArriveeDesc(sdf.parse("29/03/2021"));
+		
+		
+		Commande commande = new Commande(sdf.parse("30/03/2021"), sdf.parse("28/03/2021"), sdf.parse("29/03/2021"),
+				null);
+		commande = commandeDao.save(commande);
+
+		Commande commande1 = new Commande(sdf.parse("30/03/2021"), sdf.parse("28/03/2021"), sdf.parse("29/03/2021"),
+				null);
+		commande1 = commandeDao.save(commande1);
+
+		Commande commande2 = new Commande(sdf.parse("30/03/2021"), sdf.parse("28/03/2021"), sdf.parse("29/03/2021"),
+				null);
+		commande2 = commandeDao.save(commande2);
+
+		List<Commande> commandes = commandeDao.findByDateArriveeDesc(sdf.parse("29/03/2021"));
+		System.out.println(commandes);
+		assertEquals(3, commandes.size() - commandes2.size());
+
+	}
+	
+	@Test
+	public void findByDateArriveeAsc() throws ParseException {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		List<Commande> commandes2 = commandeDao.findByDateArriveeAsc(sdf.parse("29/03/2021"));
+		
+		
+		Commande commande = new Commande(sdf.parse("30/03/2021"), sdf.parse("28/03/2021"), sdf.parse("29/03/2021"),
+				null);
+		commande = commandeDao.save(commande);
+
+		Commande commande1 = new Commande(sdf.parse("30/03/2021"), sdf.parse("28/03/2021"), sdf.parse("29/03/2021"),
+				null);
+		commande1 = commandeDao.save(commande1);
+
+		Commande commande2 = new Commande(sdf.parse("30/03/2021"), sdf.parse("28/03/2021"), sdf.parse("29/03/2021"),
+				null);
+		commande2 = commandeDao.save(commande2);
+
+		List<Commande> commandes = commandeDao.findByDateArriveeAsc(sdf.parse("29/03/2021"));
+		System.out.println(commandes);
+		assertEquals(3, commandes.size() - commandes2.size());
+
+	}
+
+
 
 }

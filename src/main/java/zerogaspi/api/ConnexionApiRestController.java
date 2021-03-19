53714e9806f2764f5a30a72c6d2f8f0c20a88e87
Yusuf GUNER <yusuf_guner@msn.com>
2021-03-19 12:28:54 +0100

    Retrait des commentaires dans le controller Connexion

--- src/main/java/zerogaspi/api/ConnexionApiRestController.java
+++ src/main/java/zerogaspi/api/ConnexionApiRestController.java
@@ -31,7 +31,6 @@ import zerogaspi.model.Connexion;
 import zerogaspi.model.IViews;
 
 @RestController
-// Virer "/api"
 @RequestMapping("/connexion")
 public class ConnexionApiRestController {
 	@Autowired
@@ -59,9 +58,6 @@ public class ConnexionApiRestController {
 
 	@PostMapping("")
 	@JsonView(IViews.IViewConnexion.class)
-	// Ajouter @Valid @RequestBody sur le premier paramètre
-	// Ajouter BindingResult result en 2e paramètre
-	// Ajouter le if avant l'objet du modele
 	public Connexion create(@Valid @RequestBody Connexion Connexion, BindingResult result) {
 		if (result.hasErrors()) {
 			StringJoiner errors = new StringJoiner("\n");
@@ -77,8 +73,6 @@ public class ConnexionApiRestController {
 
 	@PutMapping("/{id}")
 	@JsonView(IViews.IViewConnexion.class)
-	// Ajout @Valid en premier parametre
-	// Ajouter BindingResult result en 2e paramètre
 	public Connexion update(@Valid @RequestBody Connexion Connexion, @PathVariable Long id, BindingResult result) {
 		if (!connexionDao.existsById(id) || !id.equals(Connexion.getId())) {
 			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Unable to find resource");

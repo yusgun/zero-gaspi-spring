321be18b3372b1b3873661995d17def5e938402c
bilel <bilel.boukherbache@outlook.fr>
2021-03-19 11:46:07 +0100

    json view pas utilisé , mis en commentaire

--- src/main/java/zerogaspi/model/IViews.java
+++ src/main/java/zerogaspi/model/IViews.java
@@ -6,15 +6,15 @@ public interface IViews {
 	
 	public static interface IViewAssociation extends IViewBasic {}
 	
-	public static interface IViewAssociationDetail extends IViewAssociation {}
+	//public static interface IViewAssociationDetail extends IViewAssociation {}
 	
 	public static interface IViewClient extends IViewBasic {}
 	
-	public static interface IViewClientDetail extends IViewClient {}
+	//public static interface IViewClientDetail extends IViewClient {}
 	
 	public static interface IViewCommande extends IViewBasic {}
 	
-	public static interface IViewCommandeDetail extends IViewCommande {}
+	//public static interface IViewCommandeDetail extends IViewCommande {}
 	
 	public static interface IViewCommandeWithLot extends IViewCommande {}
 	
@@ -30,51 +30,51 @@ public interface IViews {
 	
 	public static interface IViewConnexion extends IViewBasic {}
 	
-	public static interface IViewConnexionDetail extends IViewConnexion {}
+	//public static interface IViewConnexionDetail extends IViewConnexion {}
 	
 	public static interface IViewEntreprise extends IViewBasic {}
 	
-	public static interface IViewEntrepriseDetail extends IViewEntreprise {}
+	//public static interface IViewEntrepriseDetail extends IViewEntreprise {}
 	
 	public static interface IViewEntrepriseWithVendeur extends IViewBasic {}
 	
 	public static interface IViewFacture extends IViewBasic {}
 	
-	public static interface IViewFactureDetail extends IViewFacture {}
+	//public static interface IViewFactureDetail extends IViewFacture {}
 	
 	public static interface IViewFactureWithCommande extends IViewFacture {}
 	
 	
 	public static interface IViewIdentite extends IViewBasic {}
 	
-	public static interface IViewIdentiteDetail extends IViewIdentite {}
+	//public static interface IViewIdentiteDetail extends IViewIdentite {}
 	
 	public static interface IViewIdentiteWithConnexion extends IViewIdentite {}
 	
 	public static interface IViewListeFavori extends IViewBasic {}
 	
-	public static interface IViewListeFavoriDetail extends IViewListeFavori {}
+	//public static interface IViewListeFavoriDetail extends IViewListeFavori {}
 	
 	public static interface IViewListeFavoriWithEntreprise extends IViewListeFavori {}
 	
 	public static interface IViewLot extends IViewBasic {}
 	
-	public static interface IViewLotDetail extends IViewLot {}
+	//public static interface IViewLotDetail extends IViewLot {}
 	
 	public static interface IViewLotWithEntreprise extends IViewLot {}
 	
 	public static interface IViewPaiement extends IViewBasic {}
 	
-	public static interface IViewPaiementDetail extends IViewPaiement {}
+	//public static interface IViewPaiementDetail extends IViewPaiement {}
 	
 	public static interface IViewPaiementWithCommandePayante extends IViewPaiement {}
 	
 	public static interface IViewParticulier extends IViewBasic {}
 	
-	public static interface IViewParticulierDetail extends IViewParticulier {}
+	//public static interface IViewParticulierDetail extends IViewParticulier {}
 	
 	public static interface IViewVendeur extends IViewBasic {}
 	
-	public static interface IViewVendeurDetail extends IViewVendeur {}
+	//public static interface IViewVendeurDetail extends IViewVendeur {}
 
 }

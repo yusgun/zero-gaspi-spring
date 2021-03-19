e261c6ecec09ae722e6fcade436e2479251d0013
Alexandre <alexandrewolny@gmail.com>
2021-03-19 14:02:41 +0100

    Changement JsonView dans LotApiRestController

--- src/main/java/zerogaspi/api/LotApiRestController.java
+++ src/main/java/zerogaspi/api/LotApiRestController.java
@@ -124,7 +124,7 @@ public class LotApiRestController {
 	}
 	
 	@GetMapping("/entreprise/{id}")
-	@JsonView(IViews.IViewLot.class)
+	@JsonView(IViews.IViewLotWithEntreprise.class)
 	public List<Lot> findByEntreprise(@PathVariable Long id) {
 		if(entrepriseDao.existsById(id)) {
 			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Unable to find resource");
